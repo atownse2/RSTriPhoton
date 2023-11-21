bec8546676af5f0bd32a04514f1d9738b765e058
Austin Townsend <atownse2@nd.edu>
2023-11-21 13:13:15 -0500

    t

--- analysis/utils/sample_info.py
+++ analysis/utils/sample_info.py
@@ -10,40 +10,45 @@ import json
 script_dir = os.path.dirname(os.path.abspath(__file__))
 top_dir = os.path.dirname(os.path.dirname(script_dir))
 
+# Tuple version
+current_tuple_version = 'FlatAODv4'
+
+
+# Metadata
 lumi_dict = { '2016': 35.92,
               '2017': 41.53,
               '2018': 59.74} #fb^-1
 
 trigger_indicies = {'HLT_DoublePhoton70_v' : 0,
                     'HLT_DoublePhoton85_v' : 1,
-                    'HLT_Photon200_v' : 2}
+                    'HLT_Photon200_v' : 2} # Should read this from the file...
 
+sample_types = ['data', 'mc', 'signal']
 
-mc_subsets = {'GJets': ['GJets_HT-40To100', 'GJets_HT-100To200', 'GJets_HT-200To400', 'GJets_HT-400To600', 'GJets_HT-600ToInf'],}
-signal_filetag = 'BkkToGRadionToGGG'
+mc_subsets = {'GJets': ['GJets_HT-40To100',
+                        'GJets_HT-100To200',
+                        'GJets_HT-200To400',
+                        'GJets_HT-400To600',
+                        'GJets_HT-600ToInf'],}
+
 data_filetags = {'2016' : 'DoubleEG',
                  '2017' : 'DoubleEG',
                  '2018' : 'EGamma'}
 
+signal_filetag = 'BkkToGRadionToGGG'
+
+# Mass Grid
 BKK_MASS = [140, 160, 180, 250, 500, 1000, 3000]
 MOES = [0.04, 0.02, 0.01, 0.005, 0.0025]
 
 mass_grid = [ (M_BKK, np.round(MOE*(M_BKK/2), decimals=4)) for M_BKK in BKK_MASS for MOE in MOES]
-
-#For MC request
-BKK_MASS_MC_REQUEST = [ 200, 250, 500, 750, 1000, 1500, 2000, 2500, 3000]
-MOE_MC_REQUEST = [0.005, 0.0075, 0.01, 0.025, 0.04]
-
-# M_PHI / E_PHI = M_PHI / (M_BKK/2) = MOE ---> M_PHI = MOE * (M_BKK/2)
-mass_grid_mc_request = [ (M_BKK, MOE*(M_BKK/2)) for M_BKK in BKK_MASS_MC_REQUEST for MOE in MOE_MC_REQUEST]
 
 # XRootD redirectors
 hadoop_redirector = "root://deepthought.crc.nd.edu/"
 nd_redirector = "root://ndcms.crc.nd.edu/"
 fnal_redirector = "root://cmsxrootd.fnal.gov/"
 
-current_tuple_version = 'FlatAODv4'
-
+# Metadata functions
 def isMC(dataset):
     '''Returns true if the dataset is MC'''
     if 'data' in dataset:
@@ -56,15 +61,49 @@ def isSignal(dataset):
     '''Returns true if the dataset is signal'''
     return signal_filetag in dataset
 
+def get_xs(dataset):
+    '''Returns the cross section of the dataset'''
+    if isMC(dataset):
+        if isSignal(dataset):
+            return get_signal_cross_section(dataset)
+        else:
+            return load_xsdb_info(dataset)['xs']
+    else:
+        raise ValueError(f'Dataset {dataset} is not MC')
+
+def get_trigger_index(trigger, era):
+    """Reads triggers/trigger_names_<era>.txt and returns the index of the trigger"""
+    with open(f'{script_dir}/triggers/triggerNames_{era}.txt', 'r') as tfile:
+        trigger_names = tfile.read().splitlines()
+    return trigger_names.index(trigger)
+
+def get_n_events(filepath):
+    '''Returns the number of events in the file'''
+    filename = filepath.split('/')[-1]
+
+    if 'nanoAOD' in filename:
+        tree = 'flattenerMatching/tree'
+    elif 'MiniAOD' in filename:
+        tree = 'Events'
+    else:
+        raise ValueError('Tier must be nanoAOD or MiniAOD')
+
+    import uproot
+    try:
+        with uproot.open(filepath) as file:
+            return file[tree].num_entries # type: ignore
+    except:
+        return 0
+
 def get_signal_cross_section(filetag):
-    xs_file = f'{top_dir}/analysis/metadata/json/signal_xs.json'
+    xs_file = f'{top_dir}/analysis/metadata/xs/signal_xs.json'
     with open(xs_file, 'r') as f:
         xs = json.load(f)
     return xs[filetag]
 
 def load_xsdb_info(dataset):
     '''Loads the xsdb info from the json file'''
-    xsdb_path = f'{top_dir}/analysis/metadata/XSDB/'
+    xsdb_path = f'{top_dir}/analysis/metadata/xs/'
     xsdb_jsons = [f for f in os.listdir(xsdb_path) if '.json' in f and dataset in f]
     if len(xsdb_jsons) == 0:
         raise ValueError(f'No xsdb json found for {dataset}')
@@ -81,34 +120,8 @@ def load_xsdb_info(dataset):
 
     return xsdb_info
 
-
-
-#json io
-def load_mc_info(dType='signal'):
-    '''Loads the MC info from the json file'''
-    json_path = f'{script_dir}/json/{dType}_info.json'
-    if os.path.exists(json_path):
-        with open(json_path, 'r') as f:
-            info = json.load(f)
-    else:
-        info = None
-    return info
-
-def save_mc_info(info, dType='signal'):
-    '''Saves the MC info to the json file'''
-    json_path = f'{script_dir}/json/{dType}_info.json'
-    with open(json_path, 'w') as f:
-        json.dump(info, f, indent=4)
-
-def m_moe_to_m_m(m_moe):
-    '''Converts the m_moe to m_m'''
-    M_BKK, MOE = m_moe
-    M_R = (M_BKK/2)*MOE
-    return (M_BKK, M_R)
-
-
-#Signal functions
-def get_signal_filetag(signal_point=None, mc_request=False):
+# Signal functions
+def get_signal_filetag(signal_point=None):
     '''Returns the filetag for the signal files
     signal_point : tuple(M_BKK, M_R)
     returns : {signal_filetag}_M1-<M_BKK>_R-<M_R>'''
@@ -127,11 +140,11 @@ def get_signal_filetag(signal_point=None, mc_request=False):
 
     return filetag
 
-
-def get_all_signal_filetags():
-    '''Returns a list of all signal filetags in mass grid'''
-    return [get_signal_filetag(point) for point in mass_grid]
-
+def m_moe_to_m_m(m_moe):
+    '''Converts the m_moe to m_m'''
+    M_BKK, MOE = m_moe
+    M_R = (M_BKK/2)*MOE
+    return (M_BKK, M_R)
 
 def get_mass_point(fragment):
     '''Parse the mass point from the fragment'''
@@ -142,32 +155,29 @@ def get_mass_point(fragment):
         R = float(re.search(r'R0-(\d+p\d+|\d+)', fragment).group(1).replace('p', '.'))
         return M1, R
 
-def get_all_signal_points(era):
+def get_all_signal_points(era='2018', in_mass_grid=True):
     '''Returns all signal points in MiniAOD tier,
     including those that are not in the mass grid
     era: era of dataset default is RunIISummer20UL18'''
-    points = set()
-    filepaths = get_filelist('signal', era=era, tier='MiniAOD')
-    for filepath in filepaths:
-        fragment = filepath.split('/')[-1]
-        M1, R = get_mass_point(fragment)
-        points.add((M1, R))
-    return points
+    
+    if in_mass_grid:
+        return mass_grid
+    else:
+        points = set()
+        filepaths = get_filelist('signal', era=era, tier='MiniAOD')
+        for filepath in filepaths:
+            fragment = filepath.split('/')[-1]
+            M1, R = get_mass_point(fragment)
+            points.add((M1, R))
+        return points
 
+def get_all_signal_filetags(era='2018', in_mass_grid=True):
+    '''Returns a list of all signal filetags in mass grid'''
+    return [get_signal_filetag(point) for point in get_all_signal_points(era=era, in_mass_grid=in_mass_grid)]
 
-#I/O functions
-def get_all_mc_filetags(backgrounds):
-    '''Returns a list of all mc filetags'''
-    filetags=[]
-    for dataset in backgrounds:
-        filetags += mc_subsets[dataset]
-    return filetags
-
-def get_trigger_index(trigger, era):
-    """Reads triggers/trigger_names_<era>.txt and returns the index of the trigger"""
-    with open(f'{script_dir}/triggers/triggerNames_{era}.txt', 'r') as tfile:
-        trigger_names = tfile.read().splitlines()
-    return trigger_names.index(trigger)
+# General I/O functions
+def get_tuple_dir(sample_dir, tuple_version, year='2018'):
+    return f'/hadoop/store/user/atownse2/RSTriPhoton/{sample_dir}/{year}/{tuple_version}'      
 
 def get_datasets(dType,
                 year='2018',
@@ -193,10 +203,6 @@ def get_datasets(dType,
         raise ValueError(f'Invalid dType {dType}')
 
     return datasets
-
-sample_types = ['data', 'mc', 'signal']
-def get_tuple_dir(sample_dir, tuple_version, year='2018'):
-    return f'/hadoop/store/user/atownse2/RSTriPhoton/{sample_dir}/{year}/{tuple_version}'      
 
 def get_filelist(filetag,
                 tuple_version=current_tuple_version,
@@ -224,40 +230,21 @@ def get_filelist(filetag,
 
     return [f'{dir}/{f}' for f in filelist]
 
-
-def get_n_events(filepath):
-    '''Returns the number of events in the file'''
-    filename = filepath.split('/')[-1]
-
-    if 'nanoAOD' in filename:
-        tree = 'flattenerMatching/tree'
-    elif 'MiniAOD' in filename:
-        tree = 'Events'
-    else:
-        raise ValueError('Tier must be nanoAOD or MiniAOD')
-
-    import uproot
-    try:
-        with uproot.open(filepath) as file:
-            return file[tree].num_entries # type: ignore
-    except:
-        return 0
-
-
 if __name__ == '__main__':
     #Testing
 
     #Test XRD access
     #redirector = 'root://cmsxrootd.fnal.gov/'
     #redirector = "root://deepthought.crc.nd.edu/"
-    redirector = "root://ndcms.crc.nd.edu/"
+    # redirector = "root://ndcms.crc.nd.edu/"
 
-    era = 'RunIISummer20UL18'
+    # era = 'RunIISummer20UL18'
 
     #Test get_filelist
-    print(get_filelist('GJets', era=era))
+    # print(get_filelist('GJets', era=era))
 
 
 
     #update_signal_metadata(eras=['2016','2017','2018'], test=False)
     #update_bkg_metadata(eras=['2016','2017','2018'], bkg_list=mc_datasets, test=False)
+    pass
